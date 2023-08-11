a06e5bc91ae51ac8ccfbd3ee27471cc720039387
abdulmannan14 <mannanmaan1425@gmail.com>
2023-08-12 01:35:21 +0500

    new

--- getdinerotoday/settings/settings.py
+++ getdinerotoday/settings/settings.py
@@ -35,7 +35,6 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_DEFAULT_ACL = None
 
-
 DEFAULT_LOGGING['handlers']['console']['filters'] = []
 
 INSTALLED_APPS = [
